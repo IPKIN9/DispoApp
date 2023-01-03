e9b7f4360f6226c4e7380a7ad935b1f953920385
unknown <nzrkodi@gmail.com>
2023-01-03 12:10:51 +0800

    create database

---
+++ database/migrations/2023_01_02_074859_create_tikets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTiketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tikets', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_tiket');
+            $table->foreignId('id_mahasiswa')->constrained('mahasiswa');
+            $table->foreignId('id_staff')->constrained('staff');
+            $table->string('keterangan');
+            $table->string('verifikasi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tikets');
+    }
+}
